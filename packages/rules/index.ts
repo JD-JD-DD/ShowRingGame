c2f1e493c6a447661bfd98d4d7e21ad51a19e477
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-03-07 18:00:18 -0700

    fix rules workspace export. maybe.

--- packages/rules/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./engines/dog.engine";
-export * from "./engines/litter.engine";
-export * from "./engines/breeding.engine";
-export * from "./engines/trait.engine";
---
+++ packages/rules/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./dog.engine";
+export * from "./litter.engine";
+export * from "./breeding.engine";
+export * from "./trait.engine";
