604f80b1d9d42ad5a356128f5c99ac08f312df8d
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-03-13 13:00:06 -0700

    user account creation stable

---
+++ apps/web/app/onboarding/page.tsx
@@ -0,0 +1,9 @@
+export default function OnboardingPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to ShowRing Game</h1>
+      <p>Your account has been created.</p>
+      <p>Next step: create your kennel.</p>
+    </div>
+  );
+}
--- apps/web/lib/db.ts
+++ apps/web/lib/db.ts
@@ -1,13 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined;
 }
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set.");
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
 export const db =
   global.__prisma ??
   new PrismaClient({
+    adapter,
     log: ["warn", "error"],
   });
 
