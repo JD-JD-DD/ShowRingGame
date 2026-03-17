6cb42e668c1486fac3e5b2b96933acdfbe0d2f56
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-03-16 19:40:43 -0700

    fix: prisma pnpm build script in db.ts

--- apps/web/lib/db.ts
+++ apps/web/lib/db.ts
@@ -1,8 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
 
-export const db = new PrismaClient({ adapter });
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+export const db =
+  global.__prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.__prisma = db;
+}
