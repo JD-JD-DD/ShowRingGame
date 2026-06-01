3edd019055bdb1db27c7a8d6d1a68fc09bd143b1
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-06-01 13:03:25 -0700

    Remove unused placeholder rule constants

--- packages/rules/constants/training.constants.ts
+++ packages/rules/constants/training.constants.ts
@@ -1,12 +1,3 @@
 // training / conditioning
 
-export const SESSIONS_PER_DAY = 20
-
-
-// fatigue system
-
-// TBD
-export const FATIGUE_DECAY = 0 // TBD
-export const MAX_SESSIONS_PER_DOG = 0 // TBD
-export const DIMINISHING_RETURN_RATE = 0 // TBD
-export const CONDITIONING_GAIN = 0 // TBD
+export const SESSIONS_PER_DAY = 20;
