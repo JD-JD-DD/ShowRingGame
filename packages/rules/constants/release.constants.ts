1960c2f7f9acedb5dc355848a9dc78f913e8c70f
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-07-02 05:53:31 -0700

    release all breeds

--- packages/rules/constants/release.constants.ts
+++ packages/rules/constants/release.constants.ts
@@ -1,2 +1,2 @@
 
-export const CURRENT_BREED_RELEASE = 3; // Highest breed release version available to players.
+export const CURRENT_BREED_RELEASE = 17; // Highest breed release version available to players.
