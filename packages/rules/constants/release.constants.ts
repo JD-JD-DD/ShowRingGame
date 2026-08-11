45bf0ac24b6e9cf214f21a9009f90d4f12335bb2
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-08-10 17:12:24 -0700

    add: 10 new breeds

--- packages/rules/constants/release.constants.ts
+++ packages/rules/constants/release.constants.ts
@@ -1,2 +1,2 @@
 
-export const CURRENT_BREED_RELEASE = 17; // Highest breed release version available to players.
+export const CURRENT_BREED_RELEASE = 18; // Highest breed release version available to players.
