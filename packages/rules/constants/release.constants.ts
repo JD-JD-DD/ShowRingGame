ad1fbfb922e3d874ecdf4ac479f2a686477afe68
JD-JD-DD <juliedrogsvold@arizona.edu>
2026-08-11 07:52:31 -0700

    add breed release 19

--- packages/rules/constants/release.constants.ts
+++ packages/rules/constants/release.constants.ts
@@ -1,2 +1,2 @@
 
-export const CURRENT_BREED_RELEASE = 18; // Highest breed release version available to players.
+export const CURRENT_BREED_RELEASE = 19; // Highest breed release version available to players.
